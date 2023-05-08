5eb8b6d4e3debf0865dcc6df4a672037997413a1
JoshStout <79609659+JoshStout@users.noreply.github.com>
2023-05-07 19:22:25 -0500

    Update PageAndOffset.py

--- PageAndOffset.py
+++ PageAndOffset.py
@@ -1,9 +1,4 @@
 # 3/5/23
-# Assignment 4
-# This program outputs the page number and offset for three specific 
-# addresses in a system with 23-bit virtual address with 4-KB page 
-# size.
-
 
 # the three addresses given in the assignment directions
 addr1 = 19986
